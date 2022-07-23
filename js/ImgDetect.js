597df1968ec7cae87390a3915e928a005f84b861
Cyclmobile <95096495+Cyclmobile@users.noreply.github.com>
2022-07-23 23:38:58 +0200

    feat(elseIfForDrop)

--- js/ImgDetect.js
+++ js/ImgDetect.js
@@ -129,30 +129,34 @@ video.setAttribute('playsinline', '');
                 
                 console.log(prediction[1].className +  " " + prediction[1].probability.toFixed(2));
                 
-                     if (recycled >= 0.90){
+                       if (recycled >= 0.90){
                    window.location.href="completrecycl.html"
-                   return false;//prevent form submission
-                  } else if (Stn > 0.00){
-                      RecycleInfo.innerHTML='Please hold the bottle top of the hole'
-                      DropLight.style.borderColor='red';
-                      return false;//prevent form submission
-                    } else if (TooFar > 0.00){
-                      RecycleInfo.innerHTML= 'Move the camera closer to your hand &#128721'
-                      DropLight.style.borderColor='red';
-                      return false;//prevent form submission
-                    } else if (bottleOut >= 0.05){
-                          RecycleInfo.innerHTML='Hold the bottle from the side'
-                          DropLight.style.borderColor='red';
-                          return false;//prevent form submission
-                    } else if (hold >= 1.00){
-                        setTimeout(() => {
-                            RecycleInfo.innerHTML='Drop your bottle carefully &#128516; &#9851 <br> *Take your hand away after drop'
-                            DropLight.style.borderColor='green';
-                            return false;//prevent form submission
-                            
-                        }, 3000);
-                
-                    }
+
+                   if (Stn > 0.00){
+                    RecycleInfo.innerHTML='Please hold the bottle top of the hole'
+                    DropLight.style.borderColor='red';
                     return false;//prevent form submission
+                  } else if (TooFar > 0.00){
+                    RecycleInfo.innerHTML= 'Move the camera closer to your hand &#128721'
+                    DropLight.style.borderColor='red';
+                    return false;//prevent form submission
+                  } else if (bottleOut >= 0.05){
+                        RecycleInfo.innerHTML='Hold the bottle from the side'
+                        DropLight.style.borderColor='red';
+                        return false;//prevent form submission
+                  } else if (hold >= 1.00){
+                      setTimeout(() => {
+                          RecycleInfo.innerHTML='Drop your bottle carefully &#128516; &#9851 <br> *Take your hand away after drop'
+                          DropLight.style.borderColor='green';
+                          return false;//prevent form submission
+                          
+                      }, 3000);
+              
+                  }
+                      return false;//prevent form submission
+                  } else
+                  {
+                    console.log('cant recycle')
+                  }
         }
     }
